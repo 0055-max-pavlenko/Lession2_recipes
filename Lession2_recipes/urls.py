93778674cb31ba2a7bf2e7bd8d26beaf27d969ad
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-06-12 18:20:30 +0300

    ver1 12.06.2023

--- Lession2_recipes/urls.py
+++ Lession2_recipes/urls.py
@@ -1,30 +1,31 @@
-"""
+﻿"""
 Definition of urls for Lession2_recipes.
 """
 
-from datetime import datetime
+"""recipes URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.urls import path
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
-from app import forms, views
+
+from calculator.views import recipe_view, home_view, DATA
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='app/login.html',
-             authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('admin/', admin.site.urls),
+    path('', home_view),
+    path('<recipe>/', recipe_view),
+    path('admin/', admin.site.urls)
 ]
---
+++ calculator/views.py
@@ -0,0 +1,49 @@
+﻿from django.shortcuts import render
+
+
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    }
+    
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def recipe_view(request, recipe):
+    template_name = 'calculator/index.html'
+    servings = int(request.GET.get("servings", 1))
+   
+    if recipe in DATA:
+        context = {
+            'recipe': {key:value*servings for (key,value) in DATA[recipe].items()}
+        }
+    else: 
+        context = None
+    return render(request, template_name, context)
+
+def home_view(request):
+    template_name = 'calculator/home.html'
+    return render(request, template_name)
+
